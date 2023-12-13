f6f8e6475593e53133bdfc9ecd51be9a5231d166
wgp-code <839427924@qq.com>
2023-12-13 17:27:09 +0800

    fix:添加配置文件

--- Src/Common/ConfigManager.cs
+++ Src/Common/ConfigManager.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Common
 {
-    internal class ConfigManager
+    /// <summary>
+    /// 配置项管理器
+    /// </summary>
+    public static class ConfigManager
     {
+        public static string ConnectionString { get; set; } = "Server=43.135.153.183;Database=kohler_auto;Uid=kohler_auto;Pwd=kohler@2023;";
     }
 }
